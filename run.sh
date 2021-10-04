71862a7a4b7d56f248a33200ca77ce6c7f5aec98
Benoit Bouillard <benoit.bouillard@gmail.com>
2021-10-04 23:28:35 +0200

    Add ranking_dep.py in run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 git pull
 pip3 install -r requirements.txt
 python3 get_users_activities.py
-python3 challenge.py -c 2021 -i 9
+python3 challenge.py -c 2021 -i 10
 python3 ranking_fill_zones.py
 python3 ranking_fr_stats.py
+python3 ranking_dep.py
 python3 community_fill_zones.py
 python3 users_report.py
 
